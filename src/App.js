213b6ad4782c79aeb1ccad7e85e9428a30566576
Pranjal Walia <pranjalwalia77@gmail.com>
2020-12-10 17:11:54 +0530

    async fetched from the api and rendered the data

--- src/App.js
+++ src/App.js
@@ -1,36 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import { FormControl, MenuItem, Select } from "@material-ui/core";
 
 function App() {
-  const [countries, setCountries] = useState();
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+  //! componentDidMount() alternative i.e. runs when component loads or state changes
+  //! useEffect( () => , [countries])
+
+  //! we just wanna fire once in the start and never again
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((res) => res.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+            id: country._id,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = (e) => {
+    const countryCode = e.target.value;
+    setCountry(countryCode);
+  };
 
   return (
     <div className="app">
-      {/* header */}
-      {/* title  + select dropdown field */}
       <div className="app__header">
         <h1>DashBoard Covid-19</h1>
-
         <FormControl className="app__dropdown">
-          <Select
-            variant="outlined"
-            value="abc"
-            onChange={() => console.log(123)}
-          >
-            {/* loop through countries and create a dropdown for each, i.e. use component state */}
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide" key={country.id}>
+              WorldWide
+            </MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value} key={country.id}>
+                {country.name}
+              </MenuItem>
+            ))}
           </Select>
         </FormControl>
       </div>
-      {/* infoBox */}
-      {/* infoBox */}
-      {/* infoBox */}
-
-      {/* table */}
-      {/* graph */}
-
-      {/* map */}
     </div>
   );
 }
