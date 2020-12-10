68d46c70eb74367b0386632d867ea2988c1662dd
Pranjal Walia <pranjalwalia77@gmail.com>
2020-12-10 15:12:17 +0530

    WireFraming init, added a basic componen tstructure to the project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ function App() {
   return (
     <div className="app">
       <h1>the app component</h1>
+
+      {/* header */}
+      {/* title  + select dropdown field */}
+
+      {/* infoBox */}
+      {/* infoBox */}
+      {/* infoBox */}
+
+      {/* table */}
+      {/* graph */}
+
+      {/* map */}
     </div>
   );
 }
